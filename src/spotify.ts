1bdecbaf37df3999782d2a0f9efdfe0886d1ac3c
MatievisTheKat <matievisthekat@gmail.com>
2021-01-14 11:23:30 +0200

    refreshing access token is working

--- src/spotify.ts
+++ src/spotify.ts
@@ -2,17 +2,17 @@ import Axios from "axios";
 import fs from "fs-extra";
 import qs from "querystring";
 import path from "path";
-import config from "../config.json";
 import { TokenResponse, PlayerState } from "../types";
 
-const basic = Buffer.from(`${config.client_id}:${config.client_secret}`).toString("base64");
+const configPath = path.join(__dirname, "..", "config.json");
+const basic = Buffer.from(`${require(configPath).client_id}:${require(configPath).client_secret}`).toString("base64");
 const API = "https://api.spotify.com/v1";
 
 export function getPlayerState() {
   return new Promise<PlayerState>((resolve, reject) => {
     Axios.get<PlayerState>(`${API}/me/player`, {
       headers: {
-        Authorization: `Bearer ${require("../config").access_token}`,
+        Authorization: `Bearer ${require(configPath).access_token}`,
       },
     })
       .then((res) => resolve(res.data))
@@ -21,8 +21,10 @@ export function getPlayerState() {
 }
 
 export function isTokenExpired() {
-  const expiresMs = config.expires_in * 1000;
-  const fetched = config.fetched_timestamp;
+  delete require.cache[require.resolve(configPath)];
+
+  const expiresMs = require(configPath).expires_in * 1000;
+  const fetched = require(configPath).fetched_timestamp;
   const now = Date.now();
 
   return fetched + expiresMs <= now;
@@ -32,7 +34,7 @@ export function refreshToken() {
   return new Promise<void>((resolve, reject) => {
     const data = {
       grant_type: "refresh_token",
-      refresh_token: config.refresh_token,
+      refresh_token: require(configPath).refresh_token,
     };
 
     Axios.post<TokenResponse>("https://accounts.spotify.com/api/token", qs.stringify(data), {
@@ -43,18 +45,18 @@ export function refreshToken() {
       .then(async (res) => {
         const { access_token, expires_in } = res.data;
         const fetched_timestamp = Date.now();
-        const configPath = path.resolve("config.json");
 
-        await fs.writeJSON(configPath, {
-          client_id: config.client_id,
-          client_secret: config.client_secret,
-          access_token,
-          refresh_token: config.refresh_token,
-          expires_in,
-          fetched_timestamp,
-        });
-
-        delete require.cache[path.join(__dirname, "..", "config.json")];
+        await fs.writeFile(
+          configPath,
+          JSON.stringify({
+            client_id: require(configPath).client_id,
+            client_secret: require(configPath).client_secret,
+            access_token,
+            refresh_token: require(configPath).refresh_token,
+            expires_in,
+            fetched_timestamp,
+          })
+        );
 
         resolve();
       })
